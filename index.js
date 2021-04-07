5139869ee3de7ddc6eaa1d65e64898759d6920a1
Douglas Rosa <douglas.rosa@appgeolife.com>
2021-04-07 12:27:42 -0300

    print error

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.printError = function(str = '') {
-	console.log(`\n\n\n#### ERROR: ${str} ####\n\n\n`);
+exports.printError = function(err = '') {
+	console.log('\n\n\n');
+	console.log('###############');
+	console.log('#### ERROR ####');
+	console.log('###############');
+	console.log(`${err}\n\n\n`);
 }
